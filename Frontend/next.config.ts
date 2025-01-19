17707689033968b489c0dc933020620e87598704
Stepan Haida <shaida@perpet.io>
2025-01-20 00:10:48 +0200

    misc/frontend: small changes

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "upload.wikimedia.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
