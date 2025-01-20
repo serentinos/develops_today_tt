36c1305c0e8200b9dc478139a6b900fbbd6485b8
Stepan Haida <shaida@perpet.io>
2025-01-20 11:40:12 +0200

    env changes

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8088",
+  },
   images: {
     remotePatterns: [
       {
